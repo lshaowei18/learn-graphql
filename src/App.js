bd923b5b5094c912074c8b05cdcd6fbd95f9b2a1
Shao Wei <lshaowei18@gmail.com>
2020-10-05 11:14:25 +0800

    feat: fetch data from github, error handling, display basic info

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 require('dotenv').config()
@@ -12,18 +12,59 @@ const GITHUB_AXIOS_CLIENT = axios.create({
   }
 })
 
-const fetchDataFromGithub = async (url) => {
-  console.log( await GITHUB_AXIOS_CLIENT.post('', {}))
+const GET_ORGANIZATION = `
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+    }
+  }
+`
+
+const fetchDataFromGithub = () => {
+  return GITHUB_AXIOS_CLIENT.post('', {
+    query: GET_ORGANIZATION
+  })
 }
 
 // Get token from env var and create axios client
 // Create react component
 // Make graphql call
 const App = () => {
+
+  const [organization, setOrganization] = useState(null)
+  const [errors, setErrors] = useState(null)
+
+  const fetchAndUpdateOrganization = async () => {
+    const result = await fetchDataFromGithub()
+    setErrors(result.data.errors)
+    setOrganization(result.data.data.organization)
+  }
+
+  useEffect(() => {
+    fetchAndUpdateOrganization()
+  }, [])
+
+  const renderOrganization = () => {
+    if (errors) {
+      return (
+        <div>
+          <h2>Failed to fetch issues from Github.</h2>
+          <ul>
+            {errors.map(error => <li>{error.message}</li>)}
+          </ul>
+        </div>
+      )
+    }
+    if (organization) {
+      return <Organization organization={organization}/>
+    }
+    return 'Loading...'
+  }
+  
   const onSubmit = async (event) => {
     event.preventDefault()
     await fetchDataFromGithub('hi')
-    console.log("submitted!")
   }
   return (
     <div>
@@ -33,6 +74,19 @@ const App = () => {
         <input id="urlInput" type="text"/>
         <input type="submit" value="Submit"/>
       </form>
+      {renderOrganization()}
+    </div>
+  )
+}
+
+const Organization = ({ organization }) => {
+  return (
+    <div>
+      <h2>Issues from</h2>
+      <p>
+        <strong>Organization: </strong>
+        <a href={organization.url}>{organization.name}</a>
+      </p>
     </div>
   )
 }
